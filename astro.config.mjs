e83996ed18e912f464f4794b46a1dc66198161a5
ItsMeNoobieboy <54125080+ItsMeNoobieboy@users.noreply.github.com>
2025-03-16 12:41:54 -0400

    Put onto domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://itsmenoobieboy.github.io",
-    base: "scrapsite",
+    site: "https://scrapsite.yanchengzhao.com",
 
     vite: {
         plugins: [tailwindcss()],
